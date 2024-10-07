8466381fcd32a6889bbc453926f6b7a5f2bf3d51
Mike Nguyen <mikeynguyen29@gmail.com>
2024-10-06 20:52:50 -0400

    console game should be good to go!

--- main.js
+++ main.js
@@ -1,38 +1,4 @@
 "use strict";
-
-
-/* # TO DO 
-
-Create a Board array inside function Game object
-    board, rows, and columns are private types.
-    Reset board
-    Set up 3 rows and 3 columns for tic tac toe for the board
-
-    
-    
-Create function displayController 
-Players will be stored as objects so it will return players as needed for game to operate
-Set player 1 with controller 'X' character and player 2 with controller 'O' character
-prompt user to select which player they want
-    If player 1 selected, X is assigned 
-    else if 
-        player 2 selected, O is assigned
-
-
-
-Create function playGame
-allow user that has taken in selected player to start game
-Set gameEnd to false
-display message to enter a row column for the gameBoard
-while gameEnd is true, 
-    prompt user to enter row, column
-    get the board to input those values in by index
-
-
-*/
-
-
-
 // First factory function, basically lets the user pick the player they wish to be.
 const players = function displayController ()
 {
@@ -73,6 +39,9 @@ const players = function displayController ()
 
 const game = function Gameboard ()
 {
+     
+    let playerOneWins = 0;
+    let playerTwoWins = 0;
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -86,30 +55,16 @@ const game = function Gameboard ()
         [[0, 0], [1, 1], [2, 2]], // Diagonal 1
         [[0, 2], [1, 1], [2, 0]], // Diagonal 2
     ];
-    
-    
-    // Points counter for both players
-    let playerOneWins = 0;
-    let playerTwoWins = 0;
 
-
-    // For error testing
-    const seeboard = () => 
-        {
-            return board;
-        }
-
-    // Reseting board for user
     const resetBoard = () => 
     {
         for (let i = 0; i < rows; i++) 
         {
-         board[i] = ['', '', '']; // Initialize with empty strings
+         board[i] = ['', '', '']; 
         }
         return board;
     }
 
-    // Setter function to get private board array
     const setBoard = () =>
     {
     for(let i = 0; i < rows; i++)
@@ -236,12 +191,9 @@ const game = function Gameboard ()
                 resetBoard();
             }
             
-
     // Return object to the calling function
     return {resetBoard, setBoard, playGame, checkWin, checkTie, seeboard};    
 }();
-
-
 
 // Set up the game
 const board = console.log(game.setBoard());  // Initialize the game board
